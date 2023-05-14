436ccd91fe32b1489b393ad7885dd263720c5a84
Siso Ngqolosi <sisoalbert@yahoo.com>
2023-05-15 01:24:28 +0200

    working on easy ocr installation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import easyocr
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
+reader = easyocr.Reader(['en'])
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -9,9 +11,12 @@ def upload_file():
         size = len(file.read())
         size_kb = size / 1000.0
         print(f'The size of the picture is {size_kb:.2f} kilobytes.')
-        response = make_response(f'The size of the picture is {size_kb:.2f} kilobytes.')
+        # response = make_response(f'The size of the picture is {size_kb:.2f} kilobytes.')
+        image_bytes = file.read()
+        result = reader.readtext(image_bytes)
+        text = '\n'.join(detection[1] for detection in result)
+        response = make_response(text)
         response.headers['Access-Control-Allow-Origin'] = '*'
-
         return response 
     else:
         return 'No file uploaded', 400
